c0ab595df318ee65afd89e91ec7b76e83f68e7af
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2015-10-03 22:59:08 +0530

    hello.js setup and working.

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,31 @@
         fixHeader();
     });
 }(window.jQuery);
+
+
+hello.init({
+    facebook: '1634502443492240',
+    google: '615042929237-ephb8jem6h6q8vf83e6d0db50qi2dmrs.apps.googleusercontent.com',
+    github: 'cdbd672cb7a4de29c76b',
+    windows: '000000004C168A78'
+});
+hello.logout();
+hello.on('auth.login', function(auth) {
+
+    // Call user information, for the given network
+    hello(auth.network).api('/me').then(function(r) {
+        // Inject it into the container
+/*        var label = document.getElementById('profile_' + auth.network);
+        if (!label) {
+            label = document.createElement('div');
+            label.id = 'profile_' + auth.network;
+            document.getElementById('profile').appendChild(label);
+        }
+        label.innerHTML = '<img src="' + r.thumbnail + '" /> Hey ' + r.name;*/
+        console.log("Network: " + auth.network + ", " + "User: " + r.name + ", " + "E-Mail: " + JSON.stringify(r) );
+
+        //TODO - Implement posting to the sheetsu API.
+    });
+
+    //TODO - Handle Error
+});
